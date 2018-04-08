75c836d26a29d085a12615b3fb404fa24e9fa242
Rofael Aleezada <me@rofael.com>
2018-04-08 17:47:08 -0500

    Create stdafx.h

---
+++ Final/stdafx.h
@@ -0,0 +1,18 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <iostream>
+#include <thread>
+#include <type_traits>
+#include <math.h>
+#include <mutex>
+#include <vector>
+#include <stack>
+#include <utility>
+#include <chrono>

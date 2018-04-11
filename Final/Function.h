e61fda66d44d6d1e491fa1368d8c531629ba2244
Rofael Aleezada <me@rofael.com>
2018-04-10 21:56:12 -0500

    Update Function.h

--- Final/Function.h
+++ Final/Function.h
@@ -6,15 +6,22 @@
 #include <math.h>
 #include <mutex>
 #include <vector>
-#include <stack>
 #include <utility>
+#include <chrono>
 
 template <typename Number>
 class Function {
     static_assert(std::is_floating_point<Number>::value, "Function only supports floating points.");
 private:
     std::vector<Number> args;
-    std::mutex lock;
+    std::vector<std::thread> threads;
+    
+    std::mutex output_lock;
+    std::mutex thread_request_lock;
+    std::mutex args_lock;
+    
+    std::atomic_int live_threads;
+    std::atomic_bool started;
     
     Number operator()(Number input) {
         Number output = 0;
@@ -28,72 +35,48 @@ private:
     }
     
     void compute(const Number a, const Number b, const Number tol, Number& I) {
-        Number f_a = (*this)(a);
-        Number f_b = (*this)(b);
+        ++live_threads;
+        if (!started) started = true;
+        Number m = (a + b) / 2;
+        Number f_a, f_b, f_m;
+        {
+            std::lock_guard<std::mutex> guard(args_lock);
+            f_a = (*this)(a);
+            f_b = (*this)(b);
+            f_m = (*this)(m);
+        }
         
         Number I1 = ((b - a) / 2)*(f_a + f_b);
-        
-        Number m = (a + b) / 2;
-        Number f_m = (*this)(m);
-        
         Number I2 = ((b - a) / 4)*(f_a + 2 * f_m + f_b);
         
         if (abs(I1 - I2) < 3 * (b - a)*tol) {
-            std::lock_guard<std::mutex> guard(lock);
+            std::lock_guard<std::mutex> guard(output_lock);
             I += I2;
         }
         else {
-            std::thread left(&Function::compute, this, std::cref(a), std::cref(m), std::cref(tol), std::ref(I));
-            std::thread right(&Function::compute, this, std::cref(m), std::cref(b), std::cref(tol), std::ref(I));
-            
-            left.join();
-            right.join();
+            std::lock_guard<std::mutex> guard(thread_request_lock);
+            threads.emplace_back(&Function::compute, this, a, m, tol, std::ref(I));
+            threads.emplace_back(&Function::compute, this, m, b, tol, std::ref(I));
         }
+        --live_threads;
     }
+    
 public:
-    Function(const std::initializer_list<Number> arguments) {
-        args = std::vector<Number>(arguments);
-    }
-    
-    Number integrateSingle(const Number lower, const Number upper, const Number tolerance) {
-        Function f = *this;
-        
-        std::stack<std::pair<Number, Number>> stack;
-        stack.push(std::pair<Number, Number>(lower, upper));
-        Number I = 0;
-        
-        while (!stack.empty()) {
-            auto pair = stack.top();
-            stack.pop();
-            Number a = pair.first;
-            Number f_a = f(a);
-            Number b = pair.second;
-            Number f_b = f(b);
-            
-            Number I1 = ((b - a) / 2)*(f_a + f_b);
-            
-            Number m = (a + b) / 2;
-            Number f_m = f(m);
-            
-            Number I2 = ((b - a) / 4)*(f_a + 2 * f_m + f_b);
-            
-            if (abs(I1 - I2) < 3 * (b - a)*tolerance) {
-                I += I2;
-            }
-            else {
-                stack.push(std::pair<Number, Number>(a, m));
-                stack.push(std::pair<Number, Number>(m, b));
-            }
-        }
-        
-        return I;
-    }
-    
+    Function(const std::initializer_list<Number> arguments) : args(arguments) {}
+
     Number integrate(const Number lower, const Number upper, const Number tolerance) {
         Number I = 0;
-
-        std::thread(&Function::compute, this, std::cref(lower), std::cref(upper), std::cref(tolerance), std::ref(I)).join();
+        live_threads = 0;
+        started = false;
         
+        threads.emplace_back(&Function::compute, this, lower, upper, tolerance, std::ref(I));
+
+        while (live_threads || !started) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(live_threads));
+        }
+        
+        for (auto& t : threads) t.join();
+
         return I;
     }
     

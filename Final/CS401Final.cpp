7b008a4af5dabd9e6bb5ea7498ebd392a71adb48
Rofael Aleezada <me@rofael.com>
2018-04-26 11:36:20 -0500

    Add files via upload

--- Final/CS401Final.cpp
+++ Final/CS401Final.cpp
@@ -1,20 +1,19 @@
 #include "Function.h"
 
 // Windows: Add to VS Project
-// Mac/Linux: clang++ -std=c++11 CS401Final.cpp -o CS401Final (make sure Function.h is in the same folder)
-// Linux: g++ -std=c++11 CS401Final.cpp -o CS401Final (make sure Function.h is in the same folder)
+// Mac/Linux: Use the Makefile
 
 // Instructions: Create a const std::function<Number(const Number)>
 // Change variables of integrate to represent the lower and upper bounds and the tolerance
 
 int main()
 {
-    const std::function<double(const double)> func = [](const double input) { return pow(input, 2) + 2*input + 1;};
+    const std::function<double(const double)> func = [](const double input) {
+        double output = 0;
+        for (int i = 11; i >= 0; --i) output += i*pow(input,i);
+        return output;
+    };
     
-    std::cout << integrate<double>(func, 2, 5, 0.01) << std::endl;
-    
-#ifdef _WIN32 || WIN32
-    system("pause");
-#endif
+    std::cout << integrate<double>(func, 2, 5, 100) << std::endl;
     return 0;
 }
--- Final/Function.h
+++ Final/Function.h
@@ -1,14 +1,15 @@
 #pragma once
 
+#include <atomic>
+#include <chrono>
 #include <iostream>
-#include <thread>
-#include <type_traits>
 #include <math.h>
 #include <mutex>
-#include <chrono>
+#include <thread>
+#include <type_traits>
 
 template <typename Number>
-static void compute(const std::function<Number(const Number)> func, const double a, const double b, const double tol, double& I, std::atomic_int& live_threads, std::mutex& output_lock) {
+static void compute(const std::function<Number(const Number)> func, const double a, const double b, const double tol, double& I, std::atomic_int& live_threads, std::mutex& lock, std::atomic_bool& has_failed) {
     double m = (a+b)/2;
     double f_a = func(a);
     double f_b = func(b);
@@ -19,32 +20,49 @@ static void compute(const std::function<Number(const Number)> func, const double
     
     if (abs(I1 - I2) < 3 * (b - a) * tol) {
         {
-            std::lock_guard<std::mutex> guard(output_lock);
+            try {
+                std::lock_guard<std::mutex> guard(lock);
+            } catch(...) {
+                --live_threads;
+                return;
+            }
             I += I2;
         }
         --live_threads;
     }
     else {
-        std::thread(compute<Number>, func, a, m, tol, std::ref(I), std::ref(live_threads), std::ref(output_lock)).detach();
+        try {
+        std::thread(compute<Number>, func, a, m, tol, std::ref(I), std::ref(live_threads), std::ref(lock), std::ref(has_failed)).detach();
+        } catch(...) {
+            --live_threads;
+            has_failed = true;
+            return;
+        }
         ++live_threads;
-        compute(func, m, b, tol, I, live_threads, output_lock);
+        compute(func, m, b, tol, I, live_threads, lock, has_failed);
     }
 }
 
 template <typename Number>
 static Number integrate(const std::function<Number(const Number)> func, const Number lower, const Number upper, const Number tol) {
-    static_assert(std::is_integral<Number>::value || std::is_floating_point<Number>::value, "Type must be numeric\n");
+    static_assert(std::is_integral<Number>::value || std::is_floating_point<Number>::value, "Type must be a number");
     
     double I = 0;
     std::atomic_int live_threads;
+    std::atomic_bool has_failed;
     live_threads = 0;
+    has_failed = false;
     
-    std::mutex output_lock;
+    std::mutex lock;
     
-    std::thread(compute<Number>, func, lower, upper, tol, std::ref(I), std::ref(live_threads), std::ref(output_lock)).detach();
+    std::thread(compute<Number>, func, lower, upper, tol, std::ref(I), std::ref(live_threads), std::ref(lock), std::ref(has_failed)).detach();
     ++live_threads;
     
     while (live_threads) {
+        if (has_failed) {
+            std::cout << "Thread limit reached: Try increasing the tolerance." << std::endl;
+            return 0;
+        }
         std::this_thread::sleep_for(std::chrono::milliseconds(live_threads));
     }
     
